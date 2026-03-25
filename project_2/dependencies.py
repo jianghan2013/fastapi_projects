b38cbcdb1506ec66dbe1d1aafd6ea25480373545
Han Jiang <jianghan2013@gmail.com>
2026-03-24 20:46:30 -0500

    make adjustment

--- project_2/dependencies.py
+++ project_2/dependencies.py
@@ -1,6 +1,9 @@
 from fastapi import HTTPException
 from .fake_data import case_db, user_db
 from .data_models import Case, User
+from typing import Annotated
+from fastapi import Depends
+
 
 def get_valid_case(case_id: int) -> Case:
     if case_id in case_db:
@@ -14,3 +17,8 @@ def get_valid_user(user_id: int) -> User:
         return User(**user_db[user_id])
     else:
         raise HTTPException(status_code=404, detail="User not found")
+# user -> get_valid_user -> case -> Case -> get_valid_case
+def get_user_from_case(
+    case: Annotated[Case, Depends(get_valid_case)]
+    ) -> User:
+    return get_valid_user(case.user_id)
--- project_2/routers/cases.py
+++ project_2/routers/cases.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter
 from typing import Annotated
-from ..dependencies import get_valid_case, get_valid_user
-from ..data_models import Case, NotesCreate, ActionCreate
+from ..dependencies import get_valid_case, get_user_from_case
+from ..data_models import Case, User, NotesCreate, ActionCreate
 from ..services.case_service import  update_case_notes, apply_case_action
 
 
@@ -24,12 +24,19 @@ async def get_case(case: Annotated[Case, Depends(get_valid_case)]):
 
 # make the notes as a request parameter instead of a query parameter    
 @router.post("/{case_id}/notes", response_model=Case)
-async def write_notes(payload: NotesCreate, case: Annotated[Case, Depends(get_valid_case)]):
-    return update_case_notes(payload, case)
+async def write_notes(
+    payload: NotesCreate,
+    case: Annotated[Case, Depends(get_valid_case)]
+):
+    return update_case_notes(case, payload)
 
 
 # this include take action as well as reinstated
 # use patch since it only update partially
 @router.patch("/{case_id}/action", response_model=Case)
-async def take_action(payload: ActionCreate, case: Annotated[Case, Depends(get_valid_case)]):
-    return apply_case_action(payload, case)
+async def take_action(
+    payload: ActionCreate,
+    case: Annotated[Case, Depends(get_valid_case)],
+    user: Annotated[User, Depends(get_user_from_case)]
+):
+    return apply_case_action(case, payload)
